9af3949d0511c0d8c9b5db149b2645c86b22a1db
David Barnett <david.barnett@maastrichtuniversity.nl>
2020-10-31 21:19:47 +0100

    fixes: plot_comp_bar handles <3 samples and no ordering

--- R/plot_comp_bar.R
+++ R/plot_comp_bar.R
@@ -1,11 +1,15 @@
 #' Pretty & flexible barplots of sample composition
 #'
-#' No gaps, unless you want them. Grouped bars and bar outlines if you want them.
-#' Consistent, distinct, fairly pleasant colouring.
+#' Stacked barplots showing composition of phyloseq samples for a specified number of coloured taxa.
+#' - sample_order: The bars are sorted by similarity, according to a specified distance measure (default aitchison), and seriation/ordering algorithm (default Ward hierarchical clustering with optimal leaf ordering.)
+#' - groups: You can group the samples on distinct plots by levels of a variable in the phyloseq object. The list of ggplots produced can be arranged flexibly with the patchwork package functions.
+#' - bar_width: No gaps between bars, unless you want them (decrease width argument).
+#' - bar_outline_colour: Bar outlines default to "black". Set to NA if you don't want outlines.
+#' - palette: Default colouring is consistent across any groups, distinct, and fairly pleasant.
 #'
 #' @param ps phyloseq object
-#' @param n_taxa how many distinct taxa to colour in the plot (otherwise "other")
 #' @param tax_level taxonomic aggregation level (from rank_names(ps))
+#' @param n_taxa how many distinct taxa to colour in the plot (otherwise "other")
 #' @param tax_order or what? taxonomy grouping?
 #' @param palette palette for taxa fill colours
 #' @param sample_order any distance measure in calc_dist that does not require a phylogenetic tree (or a variable?)
@@ -13,7 +17,7 @@
 #' @param groups splits dataset by this variable (must be categorical) and uses patchwork to assemble multiple plots?
 #' @param horizontal typically you have more samples than taxa and more vertical space than horizontal, so TRUE makes sense as default
 #' @param bar_width  default 1 avoids random gapping otherwise seen with many samples (set to something less than 1 to introduce gaps between fewer samples)
-#' @param bar_outline_colour line colour separating taxa and samples
+#' @param bar_outline_colour line colour separating taxa and samples (use NA for none)
 #' @param drop_unused_vars speeds up ps_melt but might limit future plot customisation options
 #' @param seriate_method name of any ordering method suitable for distance matrices (see ?seriation::seriate)
 #'
@@ -24,12 +28,24 @@
 #' library(microbiome)
 #' data(dietswap)
 #'
+#' # illustrative simple customised example
+#' dietswap %>%
+#'   subset_samples(timepoint == 1) %>%
+#'   plot_comp_bar(
+#'     tax_level = "Family", n_taxa = 8,
+#'     bar_outline_colour = NA,
+#'     sample_order = "bray", bar_width = 0.7
+#'   )
+#'
 #' # make a list of 2 harmonised composition plots (grouped by sex)
-#' p <- plot_comp_bar(dietswap, n_taxa = 15, bar_outline_colour = "black",
-#'                    sample_order = 'aitchison', groups = 'sex')
+#' p <- plot_comp_bar(dietswap,
+#'   n_taxa = 15, tax_level = "Genus",
+#'   bar_outline_colour = "black",
+#'   sample_order = "aitchison", groups = "sex"
+#' )
 #'
 #' # plot them side by side with patchwork package
-#' patch <- patchwork::wrap_plots(p, ncol = 2, guides = 'collect')
+#' patch <- patchwork::wrap_plots(p, ncol = 2, guides = "collect")
 #' patch
 #'
 #' # beautifying tweak #
@@ -43,34 +59,47 @@
 #' # you can theme all plots with the & operator
 #' patch & theme(axis.text.y = element_text(size = 5), legend.text = element_text(size = 6))
 #' # See https://patchwork.data-imaginist.com/index.html
-#'
-#'
 plot_comp_bar <- function(
-  ps,
-  n_taxa = 10,
-  tax_level = "Genus",
-  tax_order = "abundance",
-  palette = c("lightgrey", rev(distinct_palette(n_taxa))),
-  sample_order = "aitchison",
-  label = "SAMPLE",
-  groups = NA,
-  horizontal = TRUE,
-  bar_width = 1,
-  bar_outline_colour = 'black',
-  drop_unused_vars = TRUE,
-  seriate_method = "OLO_ward"
-){
+                          ps,
+                          tax_level,
+                          n_taxa = 8,
+                          tax_order = "abundance",
+                          palette = c("lightgrey", rev(distinct_palette(n_taxa))),
+                          sample_order = "aitchison",
+                          label = "SAMPLE",
+                          groups = NA,
+                          horizontal = TRUE,
+                          bar_width = 1,
+                          bar_outline_colour = "black",
+                          drop_unused_vars = TRUE,
+                          seriate_method = "OLO_ward") {
 
   # how many taxa to plot (otherwise group into other)
   ps <- microbiome::aggregate_top_taxa(ps, top = n_taxa, level = tax_level)
 
-  # set colour scheme
-  ordered_taxa <- rev(c(setdiff(microbiome::top_taxa(ps), "Other"), "Other"))
-  names(palette) <- ordered_taxa
-
+  # set taxa order
+  if (tax_order == "abundance") {
+    ordered_taxa <- rev(c(setdiff(microbiome::top_taxa(ps), "Other"), "Other"))
+  } else {
+    # e.g. should allow external taxa ordering from calling top_taxa on a larger superset phyloseq
+    ordered_taxa <- tax_order
+  }
+  # fix taxa colour scheme
+  if (isTRUE(is.null(names(palette)))) {
+    names(palette) <- ordered_taxa
+  }
   # determine sample ordering option
-  if(length(sample_order) == 1 && !sample_order %in% phyloseq::sample_variables(ps)){
+  samples_ordered_by_similarity <- FALSE # default (overwritten if true)
+    if (identical(sample_order, "none")){
+    ordered_samples <- phyloseq::sample_names(ps)
+  } else if (length(sample_order) == 1 && !sample_order %in% phyloseq::sample_variables(ps)) {
     samples_ordered_by_similarity <- TRUE
+  } else if (length(sample_order) > 1) {
+    ordered_samples <- sample_order
+  } else {
+    stop(sample_order,
+         " <- ordering by metadata variable levels is not yet implemented,",
+         "\n\tbut you can pass a pre-arranged list of sample names to sample_order")
   }
 
   # create a sample names variable if this will be used for labelling
@@ -92,7 +121,7 @@ plot_comp_bar <- function(
     if (isFALSE(is.na(groups))) {
       kept_vars <- c(union(kept_vars, groups))
     }
-    if (!samples_ordered_by_similarity) {
+    if (!samples_ordered_by_similarity && sample_order != "none") {
       kept_vars <- c(union(kept_vars, sample_order))
     }
     phyloseq::sample_data(ps) <- microbiome::meta(ps)[, kept_vars, drop = FALSE]
@@ -100,19 +129,22 @@ plot_comp_bar <- function(
 
 
   # define function to actually create the plot/plots
-  plot_function <- function(ps){
+  plot_function <- function(ps) {
 
     # sample ordering
     if (samples_ordered_by_similarity) {
-      # calculate distance between samples for pretty ordering
-      distMat <- ps %>%
-        # microbiome::transform(transform = "compositional") %>%
-        calc_dist(dist = sample_order) %>% .[["distMat"]]
-      ser <- seriation::seriate(x = distMat, method = seriate_method)
-      s_order <- seriation::get_order(ser)
-      ordered_samples <- attr(distMat, which = "Labels")[s_order]
+      if(phyloseq::nsamples(ps) > 2){
+        # calculate distance between samples for pretty ordering
+        distMat <- ps %>%
+          # microbiome::transform(transform = "compositional") %>%
+          calc_dist(dist = sample_order) %>% .[["distMat"]]
+        ser <- seriation::seriate(x = distMat, method = seriate_method)
+        s_order <- seriation::get_order(ser)
+        ordered_samples <- attr(distMat, which = "Labels")[s_order]
+      } else {
+        ordered_samples <- phyloseq::sample_names(ps)
+      }
     }
-
     # create long dataframe from compositional phyloseq
     df <- ps %>%
       microbiome::transform(transform = "compositional") %>%
@@ -122,9 +154,9 @@ plot_comp_bar <- function(
     df[[tax_level]] <- factor(df[[tax_level]], levels = ordered_taxa, ordered = TRUE)
 
     # set sample order
-    df[["SAMPLE"]] <- factor(df[["SAMPLE"]], levels = ordered_samples, ordered = TRUE)
+    df[["SAMPLE"]] <- factor(df[["Sample"]], levels = ordered_samples, ordered = TRUE)
 
-
+    # build plot
     p <- ggplot(df, aes_string(x = "SAMPLE", y = "Abundance", fill = tax_level))
 
     if (is.na(bar_outline_colour)) {
@@ -138,15 +170,12 @@ plot_comp_bar <- function(
       scale_x_discrete(
         # limits = ordered_samples,
         breaks = ordered_samples,
-        labels = LABELLER) +
+        labels = LABELLER
+      ) +
       scale_fill_manual(values = palette, guide = guide_legend(reverse = TRUE))
 
     if (horizontal) {
-      p <- p + coord_flip()
-    }
-
-    if(label == "SAMPLE"){
-      p <- p + xlab(NULL)
+      p <- p + xlab(NULL) + coord_flip()
     }
 
     p
@@ -154,10 +183,10 @@ plot_comp_bar <- function(
   # grouping / splitting entire phyloseq (multiple susbset_sample calls)
   LEVELS <- unique(phyloseq::sample_data(ps)[[groups]])
 
-  if(isTRUE(is.null(LEVELS))){
+  if (isTRUE(is.null(LEVELS))) {
     plot_function(ps)
   } else {
-    plots_list <- lapply(LEVELS, function(level){
+    plots_list <- lapply(LEVELS, function(level) {
       keepers <- phyloseq::sample_data(ps)[[groups]] == level
       ps <- phyloseq::prune_samples(samples = keepers, x = ps)
       plot_function(ps)
