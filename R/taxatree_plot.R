c8b55b5a198b37a3f492eb1add688374eaf66c29
David Barnett <clav1cl@hotmail.co.uk>
2021-01-28 20:54:28 +0100

    taxatree updates

--- R/taxatree_plot.R
+++ R/taxatree_plot.R
@@ -1,9 +1,9 @@
 #' Plot taxonomic stats on a ggraph tree
 #'
-#' Uses ggraph. See tree-development vignette.
+#' Uses ggraph (see help for main underlying graphing function with ?ggraph::ggraph)
+#' See tree-development vignette 2021.
 #'
-#' @param graph_object tidygraph made with taxatree_nodes taxatree_edges
-#' @param layout_coords dataframe of x and y coordinates
+#' @param graph tidygraph made with taxatree_nodes and taxatree_edges
 #' @param size_stat name of variable to scale size of nodes and edges (natural log scale)
 #' @param colour_stat name of variable to scale colour/fill of nodes and edges (natural log scale)
 #' @param max_node_size set this to avoid overlapping nodes at different scales
@@ -15,6 +15,7 @@
 #' @param reverse_colours reverse direction of colour scale?
 #' @param luminance_l2 luminance of colourscale midpoint (higher is lighter)
 #' @param colour_lims limits of colour and fill scale, used to harmonise across several plots (values squished to fit this range!)
+#' @param layout name of ggraph layout or manually specified layout with data.frame of x and y coordinates
 #'
 #' @return ggraph ggplot
 #' @export
@@ -22,8 +23,8 @@
 #' @examples
 #' #
 taxatree_plot <- function(
-                          graph_object,
-                          layout_coords,
+                          graph,
+                          preset_style = NA,
                           size_stat = "taxon_mean",
                           colour_stat = "b_mu",
                           max_node_size = 8,
@@ -31,12 +32,18 @@ taxatree_plot <- function(
                           sig_var = "p_mu",
                           sig_threshold = 0.05,
                           sig_stroke_width = 1.5,
-                          colour_palette = "Green-Brown", # currently not working inside function
-                          reverse_colours = FALSE, # currently not working inside function
-                          luminance_l2 = 80, # currently not working inside function
-                          colour_lims = c(-2, 2) # currently partly not working inside function
+                          colour_palette = "Green-Brown",
+                          reverse_colours = FALSE,
+                          luminance_l2 = 80,
+                          colour_lims = c(-3, 3),
+                          layout = "tree"
 ) {
-  p <- ggraph::ggraph(graph = graph_object, layout = layout_coords) +
+  # handle preset
+  if (!identical(preset_style, NA)){
+
+  }
+
+  p <- ggraph::ggraph(graph = graph, layout = layout, circular = TRUE) +
     ggraph::geom_edge_link(
       mapping = ggplot2::aes(
         edge_width = log(.data[[size_stat]]),
@@ -73,7 +80,7 @@ taxatree_plot <- function(
     # central black node for root
     ggraph::geom_node_point(
       mapping = ggplot2::aes(
-        filter = .data[["taxon_level"]] %in% c("kingdom","root"),
+        filter = .data[["taxon_level"]] %in% c("kingdom", "root"),
         size = log(.data[[size_stat]])
       ),
       colour = "black",
@@ -82,37 +89,50 @@ taxatree_plot <- function(
     ggplot2::scale_size_continuous(range = c(1, max_node_size), guide = "none") +
     ggraph::scale_edge_width_continuous(range = c(1, max_edge_width), guide = "none") +
 
-    # colorspace block THROWING ERRORS about can't find objects... when used INSIDE function
+    # previously colorspace block was THROWING ERRORS about can't find objects specified in its args... when used INSIDE function:
+    # solution, bizarrely, was just to use scale_colour_continuous_diverging (with aesthetics = "fill")
+    # instead of scale_fill_continuous_diverging, which fails to find the args, seemingly given the resulting location of the do.call(parent.env()) call....???
 
-    # colorspace::scale_fill_continuous_diverging(
-    #   palette = colour_palette,
-    #   l2 = luminance_l2,
-    #   aesthetics = c("edge_colour", "fill"),
-    #   limits = colour_lims,
-    #   oob = scales::oob_squish,
-    #   rev = reverse_colours,
-    #   # trans = abs_sqrt,
-    #   guide = guide_colorbar(
-    #     frame.colour = "black",
-    #     ticks.colour = "black",
-    #     barwidth = grid::unit(0.01, "npc")
-    #   )
-    # ) +
-    # # colour scale for sig. points stroke (which has no legend)
-    # colorspace::scale_color_continuous_diverging(
-    #   palette = colour_palette,
-    #   l2 = luminance_l2,
-    #   aesthetics = "color",
-    #   rev = reverse_colours,
-    #   # trans = abs_sqrt,
-    #   guide = "none"
-    # ) +
+    colorspace::scale_color_continuous_diverging(
+      palette = colour_palette,
+      l2 = luminance_l2,
+      aesthetics = c("edge_colour", "fill"),
+      limits = colour_lims,
+      oob = scales::oob_squish,
+      rev = reverse_colours,
+      trans = abs_sqrt(),
+      guide = ggplot2::guide_colorbar(
+        frame.colour = "black",
+        ticks.colour = "black",
+        barwidth = grid::unit(0.01, "npc")
+      )
+    ) +
+    # colour scale for sig. points stroke (which has no legend)
+    colorspace::scale_color_continuous_diverging(
+      palette = colour_palette,
+      l2 = luminance_l2,
+      aesthetics = "color",
+      rev = reverse_colours,
+      trans = abs_sqrt(),
+      guide = "none"
+    ) +
     ggplot2::coord_fixed(expand = FALSE, clip = "off") +
     ggraph::theme_graph(
       base_family = "sans",
       plot_margin = grid::unit(x = rep(0.03, 4), "npc")
-    ) +
-    NULL
+    )
 
   return(p)
+}
+
+abs_sqrt <- function() {
+  scales::trans_new(
+    name = "abs_sqrt",
+    transform = function(x) {
+      sign(x) * sqrt(abs(x))
+    },
+    inverse = function(x) {
+      sign(x) * x^2
+    }
+  )
 }
