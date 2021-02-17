8b99df474b5b11756cfdf2f395332b8ea26839fd
David Barnett <clav1cl@hotmail.co.uk>
2021-02-17 21:39:40 +0100

    skip ord_explore shiny test on github actions (temporarily) due to lack of phantomjs dependency install
    currently just test locally

--- tests/testthat/test-ord_explore.R
+++ tests/testthat/test-ord_explore.R
@@ -3,6 +3,7 @@ library(shinytest)
 test_that("ord_explore() works", {
   testthat::skip_on_cran()
   testthat::skip_on_bioc()
+  testthat::skip_on_ci()
 
   expect_pass(testApp(appDir = "apps/ord_explore/", compareImages = FALSE))
 })
