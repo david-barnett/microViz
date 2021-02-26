e36abde56a05331891902ff0f40c0571059417bd
David Barnett <clav1cl@hotmail.co.uk>
2021-02-26 11:01:22 +0100

    tests: skip testing ord_explore on windows and mac (only test on linux due to font differences in shiny output)

--- tests/testthat/test-ord_explore.R
+++ tests/testthat/test-ord_explore.R
@@ -3,7 +3,7 @@ library(shinytest)
 test_that("ord_explore() works", {
   testthat::skip_on_cran()
   testthat::skip_on_bioc()
-  testthat::skip_on_ci()
+  testthat::skip_on_os(os = c("windows", "mac"))
 
   expect_pass(testApp(appDir = "apps/ord_explore/", compareImages = FALSE))
 })
