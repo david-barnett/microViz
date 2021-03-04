59e81f6030bead70d887170e0fb4f34c669efdc5
David Barnett <clav1cl@hotmail.co.uk>
2021-03-04 09:05:50 +0100

    fix tests re ord_calc update

--- tests/testthat/test-ord_plot_iris.R
+++ tests/testthat/test-ord_plot_iris.R
@@ -4,7 +4,7 @@ clr_pca <- dietswap %>%
   ps_filter(timepoint %in% c(1, 2)) %>%
   tax_agg("Genus") %>%
   tax_transform("clr") %>%
-  ord_calc("PCA")
+  ord_calc(method = "PCA")
 
 test_that("ord_plot_iris stops and requests ps arg if data is noted as transformed", {
   expect_error(
