da0007c31b488a44892179e0255c4033080dcc34
David Barnett <clav1cl@hotmail.co.uk>
2021-03-04 09:04:50 +0100

    phyloseq_validate checks for short tax_table entries and gives better messages

--- R/phyloseq_validate.R
+++ R/phyloseq_validate.R
@@ -1,9 +1,11 @@
 #' Check for (and fix) common problems with phyloseq objects
 #'
 #' - Ensures the storage mode of a phyloseq object's otu table is "double" instead of e.g. "integer".
-#' - Checks for, and warns about, common uninformative entries in the tax_table which often cause unwanted results in
+#' - Checks for, and warns about, common uninformative entries in the tax_table which often cause unwanted results in.
+#' Used inside
 #'
 #' @param ps phyloseq object
+#' @param min_tax_length minimum number of characters to not consider a tax_table entry suspiciously short
 #' @param verbose print informative messages if true
 #'
 #' @return possibly modified phyloseq object
@@ -14,23 +16,28 @@
 #'
 #' phyloseq_validate(dietswap, verbose = TRUE)
 #' # no messages means no problems detected
-phyloseq_validate <- function(ps, verbose = TRUE) {
+phyloseq_validate <- function(ps, min_tax_length = 4, verbose = TRUE) {
 
   # check and fix storage mode
   if (storage.mode(phyloseq::otu_table(ps)) != "double") {
     if (verbose) {
-      message("Setting OTU table's storage.mode to 'double'")
+      message("Note: changing OTU table's storage.mode to 'double', try `ps <- phyloseq_validate(ps, verbose = FALSE)` to avoid this message")
     }
     storage.mode(phyloseq::otu_table(ps)) <- "double"
   }
 
+  # check for NULL sample data
+
+
   # check tax_table for uninformative entries
   suspicious_names <- c("g__", "f__", "unknown", "Unknown", "", " ", "NA")
+  taxfillmessage <- "Consider using tax_fill_unknowns() to help make taxa uniquely identifiable"
   if (anyNA(phyloseq::tax_table(ps))) {
-    message("NAs detected in phyloseq tax_table:\nConsider using tax_fill_unknowns() to help make taxa uniquely identifiable\n")
-  }
-  if (any(phyloseq::tax_table(ps) %in% suspicious_names)) {
-    message("Suspicious values detected in phyloseq tax_table:\nConsider using tax_fill_unknowns() to help make taxa uniquely identifiable")
+    message("NAs detected in phyloseq tax_table:\n", taxfillmessage)
+  } else if (any(nchar(phyloseq::tax_table(ps)) < min_tax_length)) {
+    message("Short values detected in phyloseq tax_table (nchar<", min_tax_length, ") :\n", taxfillmessage)
+  } else if (any(phyloseq::tax_table(ps) %in% suspicious_names)) {
+    message("Suspicious values detected in phyloseq tax_table:\n", taxfillmessage)
     message("Detected: '", paste(unique(unclass(phyloseq::tax_table(ps))[phyloseq::tax_table(ps) %in% suspicious_names]), collapse = "', '"), "'\n")
   }
 
