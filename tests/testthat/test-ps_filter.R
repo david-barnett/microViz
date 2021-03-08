bf5077d74b247cfd387739226d91946e7ecccb47
David Barnett <clav1cl@hotmail.co.uk>
2021-03-08 08:28:15 +0100

    more tests

--- tests/testthat/test-ps_filter.R
+++ tests/testthat/test-ps_filter.R
@@ -2,7 +2,6 @@ library(phyloseq)
 library(microbiome)
 data("enterotype")
 
-
 # warning if you want to .keep_all_taxa = FALSE and some of the otu_values are negative
 test_that("negative taxa gives warning", {
   expect_warning(
@@ -11,4 +10,19 @@ test_that("negative taxa gives warning", {
       ps_filter(SeqTech == "Sanger"),
     regexp = "some values in the otu_table are negative"
   )
+})
+
+test_that(desc = "ps_filter can do equivalent to ps_drop_incomplete", {
+  expect_identical(
+    object = enterotype %>% ps_filter(across(everything(), ~ !is.na(.)), .keep_all_taxa = TRUE),
+    expected = enterotype %>% ps_drop_incomplete()
+  )
+})
+
+test_that(desc = "ps_filter can do equivalent to dropping samples with incomplete sample_variables and tax_filtering 0s", {
+  ps2 <- enterotype %>% ps_filter(across(everything(), ~ !is.na(.)))
+  ps3 <- enterotype %>%
+    ps_drop_incomplete() %>%
+    tax_filter(prev_detection_threshold = 1e-20, is_counts = FALSE)
+  expect_identical(object = ps2, expected = ps3)
 })
