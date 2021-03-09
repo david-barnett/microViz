783f05f942381d9a81beb9b055ffbc5d71f59d0f
David Barnett <clav1cl@hotmail.co.uk>
2021-03-09 23:23:12 +0100

    samdat_tbl unconventional phyloseq data accessor function

--- R/ps_extra-accessors.R
+++ R/ps_extra-accessors.R
@@ -75,3 +75,23 @@ otu_get <- function(data) {
   return(otu)
 }
 
+#' @param data phyloseq or ps_extra
+# @return phyloseq sample_data as a tibble, with sample_names as new first column called .sample_name
+#' @param sample_names_col name of column where sample_names are put. if NA, return data.frame with rownames (sample_names)
+#' @rdname ps_extra-accessors
+#' @export
+samdat_tbl <- function(data, sample_names_col = ".sample_name"){
+  if (inherits(data, "ps_extra")) data <- ps_get(data)
+  if (methods::is(data, "phyloseq")) data <- phyloseq::sample_data(data)
+  if (methods::is(data, "sample_data")){
+    df <- base::data.frame(data, check.names = FALSE, stringsAsFactors = FALSE)
+  } else {
+    stop("data must be of class 'phyloseq', 'ps_extra', or 'sample_data', not: ", paste(class(data), collapse = " "))
+  }
+  if (identical(sample_names_col, NA)){
+    return(df)
+  } else {
+    df <- tibble::rownames_to_column(df, var = sample_names_col)
+    return(tibble::as_tibble(df))
+  }
+}
