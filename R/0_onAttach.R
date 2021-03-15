40a2f083a9c08b700581aeccf7260a7ba0296bd6
David Barnett <clav1cl@hotmail.co.uk>
2021-03-15 16:03:18 +0100

    shorter onattach

--- R/0_onAttach.R
+++ R/0_onAttach.R
@@ -1,16 +1,15 @@
 .onAttach = function(libname, pkgname) {
-  version = packageDescription(pkgname, fields = "Version")
+  version = utils::packageDescription(pkgname, fields = "Version")
 
-  msg = paste0("========================================
-", pkgname, " version ", version, "
---- Copyright (C) 2021 David Barnett ---
+  msg = paste0("==============================================================
+", pkgname, " version ", version, " - Copyright (C) 2021 David Barnett
 
 GitHub: https://github.com/david-barnett/microViz
 Help: https://david-barnett.github.io/microViz/
 
 This message can be suppressed by:
   suppressPackageStartupMessages(library(microViz))
-========================================
+==============================================================
 ")
 
   packageStartupMessage(msg)
