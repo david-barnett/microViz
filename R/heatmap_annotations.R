6768cb712c5b39aa2cb7da7cab27d99a5e7d30d1
David Barnett <david.barnett@maastrichtuniversity.nl>
2021-09-01 15:51:11 +0200

    tidy up heatmap annotation helpers

--- R/heatmap_annotations.R
+++ R/heatmap_annotations.R
@@ -7,14 +7,24 @@
 #' - tax_anno creates list describing taxa annotation (for cor_heatmap or comp_heatmap)
 #' - var_anno creates list describing variable annotation (for cor_heatmap)
 #'
-#' @param undetected value above which taxa are considered present/detected in a sample
-#' @param which "row" or "column" annotation, the default, NA, leaves this to be inferred by the *_heatmap function
-#' @param prev order in which prevalence annotation shown (number, or NA to not show)
-#' @param abund order in which abundance annotation shown (number, or NA to not show)
-#' @param size total size of all annotations in mm, excluding gaps (used as width or height, depending on value of which)
+#' @param undetected
+#' value above which taxa are considered present/detected in a sample
+#' @param which
+#' "row" or "column" annotation.
+#' The default, NA, leaves this to be inferred by the *_heatmap function
+#' @param prev
+#' order in which prevalence annotation shown (number, or NA to not show)
+#' @param abund
+#' order in which abundance annotation shown (number, or NA to not show)
+#' @param size
+#' total size of all annotations in mm, excluding gaps
+#' (used as width or height, depending on value of which)
 #' @param gap size of gap between annotations (in mm)
-#' @param rel_sizes relative sizes of non-NA annotations (in given order) or NA (for all equal sizes)
-#' @param args extra args passed to each annotation, give as list of lists (one inner list per arg, named, e.g. list(prev = list(whatever = whatever))
+#' @param rel_sizes
+#' relative sizes of non-NA annotations (in given order)
+#' or NA (for all equal sizes)
+#' @param args extra args passed to each annotation: give as list of lists
+#' (one inner list per arg, named, e.g. list(prev = list(whatever = whatever))
 #' @param ... further args are passed ComplexHeatmap::HeatmapAnnotation()
 #'
 #' @export
@@ -28,29 +38,36 @@ tax_anno <- function(undetected = 0,
                      rel_sizes = NA,
                      args = NULL,
                      ...) {
-  annos <- c(prev = prev, abund = abund) # written to support further annos in future
-  annos <- sort(annos[!is.na(annos)])
-  if (identical(rel_sizes, NA)) rel_sizes <- rep_len(1, length.out = length(annos))
+  # written to support further annotations in future
+  annos <- c(prev = prev, abund = abund)
+  # remove NAs
+  annos <- annos[!is.na(annos)]
+  # replace values with their order
+  annos[names(annos)] <- order(annos)
+  # sort annos as their names are used in order later
+  annos <- sort(annos)
+
+  # set default relative sizes vector as all equal
+  if (identical(rel_sizes, NA)) rel_sizes <- rep_len(1, length(annos))
 
   # check options and stop if invalid
-  if (!identical(args, NULL)) {
-    if (!inherits(args, "list") || inherits(args, "list") && length(args) != length(annos) || !names(args) %in% names(annos)) {
-      stop("args must be list of annotation-named lists, the same length as number of non-NA annotations, or NULL")
-    }
-    is_list <- sapply(args, function(x) inherits(x, "list"))
-    if (any(!is_list)) stop("some args entries are not lists: ", paste(names(args)[!is_list], collapse = " "))
+  anno_checkExtraArgsLists(args, annos)
+
+  if (length(unique(annos)) < length(annos)) {
+    stop("prev and abund must not be the same number")
   }
-  if (length(unique(annos)) < length(annos) || max(c(annos, 1) > length(annos))) {
-    stop("prev and abund must not be the same number, and neither should have a value greater than the number of non-NA entries")
-    # TODO ensure that the order of values is used instead of the values (and remove the last part of this error)
-  }
+
   if (length(rel_sizes) != length(annos)) {
-    stop("length of rel_sizes must be equal to the number of non-NA annotations")
+    stop("length of rel_sizes must equal the number of non-NA annotations")
   }
+
   # get absolute sizes from relative sizes and total
   sizes <- size * rel_sizes / sum(rel_sizes, na.rm = TRUE)
   # return partial list of instructions for taxAnnotation
-  out_list <- list(what = names(annos), undetected = undetected, which = which, sizes = sizes, gap = gap, args = args, ...)
+  out_list <- list(
+    what = names(annos), undetected = undetected,
+    which = which, sizes = sizes, gap = gap, args = args, ...
+  )
   return(out_list)
 }
 
@@ -71,7 +88,15 @@ tax_anno <- function(undetected = 0,
 #'
 #' @return HeatmapAnnotation object
 #' @noRd
-taxAnnotation <- function(data, taxa, undetected = 0, which = "row", what = c("prev", "abund"), sizes = c(15, 15), gap = 2, args = NULL, ...) {
+taxAnnotation <- function(data,
+                          taxa,
+                          undetected = 0,
+                          which = "row",
+                          what = c("prev", "abund"),
+                          sizes = c(15, 15),
+                          gap = 2,
+                          args = NULL,
+                          ...) {
   ps <- ps_get(data)
   dots <- list(...)
   # start building args list
@@ -84,7 +109,10 @@ taxAnnotation <- function(data, taxa, undetected = 0, which = "row", what = c("p
     ha_args[[name]] <- do.call(
       what = paste0("anno_", name),
       args = c(
-        list(data = ps, taxa = taxa, undetected = undetected, which = which, size = sizes[[i]]),
+        list(
+          data = ps, taxa = taxa, undetected = undetected,
+          which = which, size = sizes[[i]]
+        ),
         args[[name]]
       )
     )
@@ -97,18 +125,29 @@ taxAnnotation <- function(data, taxa, undetected = 0, which = "row", what = c("p
 
 # @param ... args passed to anno_barplot
 # @return a ComplexHeatmap anno_barplot
-#' @param data phyloseq or ps-extra (or even a data.frame or matrix for anno_var_* functions)
+#' @param data
+#' phyloseq or ps-extra (or a data.frame or matrix for anno_var_* functions)
 #' @param taxa names of taxa to plot
 #' @param size size in mm (width or height, based on which)
 #' @param bar_width relative width of barchart bars
-#' @param gp a grid::gpar() object for graphics parameter settings like fill or lwd
+#' @param gp
+#' a grid::gpar() object for graphics parameter settings like fill or lwd
 #'
 #' @export
 #' @rdname heatmap-annotations
-anno_prev <- function(data, taxa, undetected = 0, which = "row", size = 15, bar_width = 0.4, gp = grid::gpar(fill = "grey"), ...) {
+anno_prev <- function(data,
+                      taxa,
+                      undetected = 0,
+                      which = "row",
+                      size = 15,
+                      bar_width = 0.4,
+                      gp = grid::gpar(fill = "grey"),
+                      ...) {
   dots <- list(...)
   prevs <- prev_calc(data = data, taxa = taxa, undetected = undetected)
-  args <- list(x = prevs, bar_width = bar_width, ylim = 0:1, which = which, gp = gp)
+  args <- list(
+    x = prevs, bar_width = bar_width, ylim = 0:1, which = which, gp = gp
+  )
   if (identical(which, "row")) args[["width"]] <- grid::unit(size, "mm")
   if (identical(which, "column")) args[["height"]] <- grid::unit(size, "mm")
   # overwrite any args given in dots
@@ -124,10 +163,20 @@ anno_prev <- function(data, taxa, undetected = 0, which = "row", size = 15, bar_
 #'
 #' @export
 #' @rdname heatmap-annotations
-anno_abund <- function(data, taxa, undetected = 0, which = "row", size = 15, point_size = 1, box_width = 0.4, ...) {
+anno_abund <- function(data,
+                       taxa,
+                       undetected = 0,
+                       which = "row",
+                       size = 15,
+                       point_size = 1,
+                       box_width = 0.4,
+                       ...) {
   dots <- list(...)
   abunds <- abund_calc(data = data, taxa = taxa, undetected = undetected)
-  args <- list(x = abunds, size = grid::unit(point_size, "mm"), box_width = box_width, which = which)
+  args <- list(
+    x = abunds, size = grid::unit(point_size, "mm"),
+    box_width = box_width, which = which
+  )
   if (identical(which, "row")) {
     args[["x"]] <- t(abunds)
     args[["width"]] <- grid::unit(size, "mm")
@@ -144,13 +193,17 @@ anno_abund <- function(data, taxa, undetected = 0, which = "row", size = 15, poi
 
 # variables -------------------------
 
-#' @param annos name(s) of annotation(s) to show, in order (e.g. 'var_box', 'var_hist')
-#' @param funs function(s) to transform matrix of variable values before plotting (length must be 1 or same length as annos)
+#' @param annos
+#' name(s) of annotation(s) to show, in order (e.g. 'var_box', 'var_hist')
+#' @param funs
+#' function(s) to transform matrix of variable values before plotting
+#' (length must be 1 or same length as annos)
 #' @param names names to use for each annotation in annos
 #' @param which "row" or "column" annnotation
-#' @param size total size in mm of annotations (width or height depending on which)
+#' @param size total size (mm) of annotations (width/height depending on which)
 #' @param gap gap in mm between annotations
-#' @param rel_sizes relative sizes of annotations (NA for equal sizes, or same length as annos)
+#' @param rel_sizes
+#' relative sizes of annotations (NA for equal sizes, or same length as annos)
 #' @param ... further named args to be passed on (to list)
 #'
 #' @export
@@ -166,26 +219,28 @@ var_anno <- function(annos = "var_box",
                      ...) {
   # fill empty names and sizes
   if (identical(names, NA)) names <- annos
-  if (identical(rel_sizes, NA)) rel_sizes <- rep_len(1, length.out = length(annos))
+  if (identical(rel_sizes, NA)) rel_sizes <- rep_len(1, length(annos))
+
   # check matching lengths
-  if (length(funs) < length(annos)) funs <- rep_len(funs, length.out = length(annos))
+  if (length(funs) < length(annos)) funs <- rep_len(funs, length(annos))
   if (length(annos) != length(rel_sizes)) {
     stop("length of rel_sizes must be NA or equal to the length of annos")
   }
-  if (length(annos) > length(names)) stop("names must be same length as annos, or NA")
-  names <- names[seq_along(annos)] # shortens names vector if more names given than annos
-  if (!identical(args, NULL)) {
-    if (!inherits(args, "list") || inherits(args, "list") && length(args) != length(annos) || any(!names(args) %in% names)) {
-      stop("args must be list of annotation-named lists, the same length as number of non-NA annotations, or NULL")
-    }
-    is_list <- sapply(args, function(x) inherits(x, "list"))
-    if (any(!is_list)) stop("some args entries are not lists: ", paste(names(args)[!is_list], collapse = " "))
+  if (length(annos) > length(names)) {
+    stop("names must be same length as annos, or NA")
   }
+  # shorten names vector if more names given than annos
+  names <- names[seq_along(annos)]
+
+  anno_checkExtraArgsLists(args, annos)
 
   # calculate absolute sizes
   sizes <- size * rel_sizes / sum(rel_sizes, na.rm = TRUE)
   # return list
-  list(annos = annos, funs = funs, names = names, which = which, sizes = sizes, gap = gap, args = args, ...)
+  list(
+    annos = annos, funs = funs, names = names, which = which,
+    sizes = sizes, gap = gap, args = args, ...
+  )
 }
 
 #' @noRd
@@ -217,18 +272,29 @@ varAnnotation <- function(data, # from heatmap fun # converted to df
 
   for (i in seq_along(annos)) {
     name <- names[[i]]
-    if (length(funs) == 1) funs <- list(funs) # otherwise trying to subset function/closure is error
-    if (inherits(funs[[i]], "function")) dat[[i]] <- apply(mat, MARGIN = 2, FUN = funs[[i]])
-    if (inherits(funs[[i]], "character")) dat[[i]] <- apply(mat, MARGIN = 2, FUN = function(x) do.call(what = funs[[i]], args = list(x)))
+    # replace length 1 funs, otherwise subsetting function/closure --> error
+    if (length(funs) == 1) funs <- list(funs)
+    if (inherits(funs[[i]], "function")) {
+      dat[[i]] <- apply(mat, MARGIN = 2, FUN = funs[[i]])
+    }
+    if (inherits(funs[[i]], "character")) {
+      dat[[i]] <- apply(
+        X = mat, MARGIN = 2, FUN = function(x) {
+          do.call(what = funs[[i]], args = list(x))
+        }
+      )
+    }
     # check for infinite values in data
     infinite <- is.infinite(dat[[i]])
     if (any(infinite)) {
       dat[[i]][infinite] <- NA
       warning(
         sum(infinite, na.rm = TRUE),
-        " infinite values after applying funs[[", i, "]], replaced with NA in anno named: ",
-        name, "\nThis could happen when e.g. you log transform data with 0s",
-        "\nPerhaps you want 'log1p' or `function(x) log10(x + 1)` or something similar in funs argument?"
+        " infinite values after applying funs[[", i, "]]\n",
+        " - Replaced with NA in anno named: ", name, "\n",
+        " - This could happen when e.g. you log transform data with 0s\n",
+        " - Perhaps you want 'log1p' or `function(x) log10(x + 1)` \n\t",
+        "or something similar in funs argument?"
       )
     }
     # compute annotation
@@ -312,4 +378,30 @@ anno_var_box <- function(data, vars = NA, which = "column", size = 15, ...) {
   args[names(dots)] <- dots
   anno <- do.call(ComplexHeatmap::anno_boxplot, args = args)
   return(anno)
+}
+
+
+# shared helpers ----------------------------------------------------------
+
+# user can supply extra arguments to args argument for each anno, but these
+# must be supplied as a list of named lists (named by each annotation)
+anno_checkExtraArgsLists <- function(args, annos){
+  if (!identical(args, NULL)) {
+    if (!inherits(args, "list") ||
+        inherits(args, "list") && length(args) != length(annos) ||
+        any(!names(args) %in% names)) {
+      stop(
+        "\n- args can be NULL or list of named lists (named by annotation)",
+        "\n- args must be the same length as number of non-NA annotations"
+      )
+    }
+    is_list <- sapply(args, function(x) inherits(x, "list"))
+    if (any(!is_list)) {
+      stop(
+        "some args entries are not lists: ",
+        paste(names(args)[!is_list], collapse = " ")
+      )
+    }
+  }
+  return(NULL)
 }
