a4994a5977d43de83cde9a18c64d00e23086e402
David Barnett <clav1cl@hotmail.co.uk>
2021-11-17 22:34:51 +0100

    tax_fix_interactive shiny test skip due to new version of DT saving state including timestamp in json

--- tests/testthat/test-tax_fix_interactive.R
+++ tests/testthat/test-tax_fix_interactive.R
@@ -4,6 +4,13 @@ test_that("tax_fix_interactive() works", {
   testthat::skip_on_cran()
   testthat::skip_on_bioc()
   testthat::skip_on_os("windows")
+  # DT package now has timestamps in json under view_tate
+  # making shinytest snapshot testing impossible? --> skip for now
+  # https://github.com/rstudio/DT/issues/944
+  # in future, see what server function testing can do with shiny::testServer()
+  # https://shiny.rstudio.com/articles/server-function-testing.html
+  testthat::skip("Skipping since DT saves timestamp now")
+  testthat::skip_on_ci()
 
   expect_pass(testApp(appDir = "apps/tax_fix_interactive/", compareImages = FALSE))
 })
