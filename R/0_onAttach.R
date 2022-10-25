e209ef3faa5c95ace598c48b5e4a100d477812e4
David Barnett <david.barnett@maastrichtuniversity.nl>
2022-10-25 16:36:39 +0200

    hyperlink to website only in interactive session

--- R/0_onAttach.R
+++ R/0_onAttach.R
@@ -5,15 +5,20 @@
 
 rlang::on_load(rlang::local_use_cli(inline = TRUE))
 
-
 .onAttach <- function(libname, pkgname) {
   version <- utils::packageDescription(pkgname, fields = "Version")
+
+  if (interactive()) {
+    website <- "Website: {.url https://david-barnett.github.io/microViz}"
+  } else {
+    website <- "Website: https://david-barnett.github.io/microViz"
+  }
 
   rlang::inform(
     class = "packageStartupMessage",
     message = c(
       paste("{.strong", pkgname, "version", version, "- Copyright (C) 2022 David Barnett}"),
-      "!" = "Website: \t{.url https://david-barnett.github.io/microViz}",
+      "!" = website,
       "v" = 'Useful? \tFor citation details, run: {.code citation("microViz")}',
       "x" = "Silence? {.code suppressPackageStartupMessages(library(microViz))}"
     )
